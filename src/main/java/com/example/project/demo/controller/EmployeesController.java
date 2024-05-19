595ad3ad5bfca41f6abf898307d1d3a7e1401e47
Rafiul Abeer Khan <143544255+rafiulabeerkhan@users.noreply.github.com>
2024-05-20 02:51:22 +0600

    controller added

---
+++ src/main/java/com/example/project/demo/controller/EmployeesController.java
@@ -0,0 +1,51 @@
+package com.example.project.demo.controller;
+
+import com.example.project.demo.entity.Employees;
+import com.example.project.demo.repository.EmployeeRepository;
+import com.example.project.demo.service.EmployeesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+public class EmployeesController {
+
+    @Autowired
+    public EmployeesService service;
+
+    @Autowired
+    public EmployeeRepository repository;
+
+    @GetMapping("/count")
+    public Long countEmployees(){
+        return service.countEmployees();
+    }
+
+
+@GetMapping("/getAll")
+public List<Employees> getAllEmployees(){
+return service.getAllEmployees();
+}
+@PostMapping
+public Employees addEmployee( @RequestBody Employees employees){
+return service.saveEmployees(employees);
+}
+
+@GetMapping("/{id}")
+public Employees getEmployeeById(@PathVariable("id")Long id){
+    return service.getEmployeesById(id);
+}
+
+@PutMapping("/{id}")
+public Employees updateEmployees(@RequestBody Employees employees){
+    return service.saveEmployees(employees);
+}
+@DeleteMapping("/{id}")
+public void deleteEmployeeById(@PathVariable("id")Long id){
+     service.deleteEmployee(id);
+}
+
+}
